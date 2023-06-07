33c030b46a69e8ac9acc56133bd8bccf9c074ccb
Florance <Florance@localhost>
2023-06-07 18:54:43 +1000

    First commit

---
+++ mavenproject1/src/main/java/com/mavenproject1/example/Name.java
@@ -0,0 +1,9 @@
+package com.mavenproject1.example;
+
+public class Name {
+//	String name = "Florance";
+	public void show() {
+		System.out.println("Florance");
+	}
+
+}
---
+++ mavenproject1/src/main/java/com/mavenproject1/example/PrintMessage.java
@@ -0,0 +1,12 @@
+package com.mavenproject1.example;
+
+public class PrintMessage extends Name {
+
+	public static void main(String[] args) {
+		
+		Name obj = new Name();
+		obj.show();
+		
+	}
+
+}
